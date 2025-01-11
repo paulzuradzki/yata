3708a57b3dbd64e4522912693347c5323b679dac
Paul Zuradzki <paulzuradzki@gmail.com>
2025-01-11 17:55:31 -0600

    [05] TDD Green; render view from template

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
+@require_http_methods(["GET"])
 def index(request):
-    return HttpResponse("Hello world")
-
+    return render(request, "todos/index.html")

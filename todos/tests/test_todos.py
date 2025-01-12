34cfe6f4c9c9eb7f20cfcf0100e893afbf2a605c
Paul Zuradzki <paulzuradzki@gmail.com>
2025-01-11 22:10:04 -0600

    [08] test for create item; TDD Red

--- todos/tests/test_todos.py
+++ todos/tests/test_todos.py
@@ -1,5 +1,24 @@
 from django.urls import reverse
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
+
+
+def test_create_todo_item_gets_rid_of_nothing_to_see(live_server, page: Page):
+    url = reverse_url(live_server, "index")
+    page.goto(url)
+    page.get_by_label("Title*").fill("Test title")
+    page.get_by_label("Description*").fill("Test description")
+    page.get_by_role("button", name="Add").click()
+
+    expect(page.get_by_text("Nothing to see here...")).to_be_hidden()
+
+
+def test_create_todo_item_shows_new_item(live_server, page: Page):
+    url = reverse_url(live_server, "index")
+    page.goto(url)
+    page.get_by_label("Title*").fill("Test title")
+    page.get_by_label("Description*").fill("Test description")
+    page.get_by_role("button", name="Add").click()
+    page.wait_for_selector("text=foo")
 
 
 def test_display_empty_list_on_first_load(live_server, page: Page):
